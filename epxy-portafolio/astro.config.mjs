97ce7a897b4e2c871955d5281c3055937ef88324
Pap <125478255+PapLion@users.noreply.github.com>
2024-02-25 18:16:38 -0500

    Changing method (Alpha)

--- epxy-portafolio/astro.config.mjs
+++
@@ -1,9 +0,0 @@
-import image from '@astrojs/image';
-import mdx from '@astrojs/mdx';
-import tailwind from '@astrojs/tailwind';
-import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-export default defineConfig({
-	integrations: [tailwind(), mdx(), image()],
-});
--- epxy-portafolio/src/content/projects/project-1.md
+++
@@ -1,8 +0,0 @@
----
-inProgress: false
-title: Project title
-description: Project description
-img_alt: project image alt text
-link: https://github.com/veranikabarel?tab=repositories
-tags: ['React', 'CSS', 'Typescript']
----
